7996f52ca87861caae175d77ab9be83048c9b2d3
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-02-22 17:07:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let cont = document.querySelector('.container');
+for(let i=0;i<800;i++){
+	//create square boxes
+	let square = document.createElement('div');
+	square.classList.add('square');
+	cont.appendChild(square);
+	
+	square.addEventListener('mouseover',() =>{
+		square.classList.add('hovered')
+		setTimeout (() =>{
+			square.classList.remove('hovered');
+			
+		},1000);
+	});
+}
